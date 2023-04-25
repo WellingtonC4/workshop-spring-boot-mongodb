83e36ebe1ea67a717c79671c1eb4ff8f2c55349d
wellingtonc4 <wellingtontrack19@gmail.com>
2023-04-25 20:53:37 -0300

    Primeiro Commit - Projeto Criado

---
+++ src/test/java/com/wellingtonc4/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellingtonc4.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
