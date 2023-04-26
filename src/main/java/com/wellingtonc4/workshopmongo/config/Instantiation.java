d4dbf743b41d3adab703546d2f3d35fb1745366b
wellingtonc4 <wellingtontrack19@gmail.com>
2023-04-25 22:02:51 -0300

    Operação da instanciação da base de dados

---
+++ src/main/java/com/wellingtonc4/workshopmongo/config/Instantiation.java
@@ -0,0 +1,54 @@
+package com.wellingtonc4.workshopmongo.config;
+
+import java.util.Arrays;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.wellingtonc4.workshopmongo.domain.User;
+import com.wellingtonc4.workshopmongo.repository.UserRepository;
+
+@Configuration
+
+public class Instantiation implements CommandLineRunner {
+
+
+
+	@Autowired
+
+	private UserRepository userReposiroty;
+
+
+
+
+	@Override
+
+	public void run(String... arg0) throws Exception {
+
+		
+
+		userReposiroty.deleteAll();
+
+		
+
+	
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		
+
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+
+
+
+	}
+
+
+
+}
